a9f210a4297cbdd7a4babf6fb2dbc5c004811341
Guilherme Silva <106880456+guilhermesilvap@users.noreply.github.com>
2025-06-24 18:27:45 -0300

    fix: Corrigir erro de deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import { useState } from 'react'
-
-import type { Task } from './types/typing'
 import styles from "./app.module.css"
-import { ListTasks } from './components/Tasks Page'
+import { ListTasks } from "./components/Tasks Page"
 
 function App() {
- 
-    const [tasks, setTasks] = useState<Task[]>([])
-    const [modal, setModal] = useState<Task[]>([])
-
-
   return (
-
     <div className={styles.container}>
-      
-      
-
-     
- <ListTasks/> 
-       
-
-    </div>      
-    
+      <ListTasks />
+    </div>
   )
 }
 
