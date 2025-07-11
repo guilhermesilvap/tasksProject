474ad7934589fcea76110b03deabd8c79ed2207a
Guilherme Silva <106880456+guilhermesilvap@users.noreply.github.com>
2025-07-11 19:36:12 -0300

    fix: Corrigindo deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { AuthProvider } from "./contexts/AuthContext"
 import { Routes } from "./routes/index"
 
 
+
 function App() {
   return (
     <AuthProvider>
-      <Routes/>
+         <Routes/>
     </AuthProvider>
   )
 }
